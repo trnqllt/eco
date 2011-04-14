42d6642cd17946b57d55afc96381a9c60e4d53b3
Robert Wedin <robert.wedin@gmail.com>
2011-04-14 07:15:13 +0200

    da

--- server/server.js
+++ server/server.js
@@ -1,30 +1,50 @@
+// Imports
 var http = require("http");
 var bee = require("beeline");
-var api = require("api");
+var api = require("./api");
 
+// Settings
 var settings = {
     listen_ip: "0.0.0.0",
     listen_port: 8081,
     fileserver: {
-        wwwroot: "../wwwroot",
+        wwwroot: "../wwwroot/",
         defaults: ["index.html"]
     }
 }
-    
-var router = bee.route({
-    "/api/": 
 
+// Mime
+var mimes = {
+    "html": "text/html"
+    , "js": "text/javascript"
+    , "css": "text/stylesheet"
+    
+    // Images
+    , "gif": "image/gif", "png": "image/png"
+    , "jpg": "image/jpeg", "jpeg": "image/jpeg"
+
+}
+
+// Routing
+var router = bee.route({
+    "r`^/api/?.*$`": api.duh
+    , "/": bee.staticFileHandler(settings.fileserver.wwwroot + settings.fileserver.defaults[0], "text/html")
+    , "r`/(.*)`": bee.staticDirHandler(settings.fileserver.wwwroot, mimes)
 
 });
 
 
-var s = http.createServer(mainHandler);
-s.listen(settings.listen_port, settings.listen_ip);
-require("elf-logger").createLogger(s, {
+// Server
+var server = http.createServer(router);
+server.listen(settings.listen_port, settings.listen_ip);
+
+
+// W3C Logging
+require("elf-logger").createLogger(server, {
     dir: "/home/trnqllt/programming/code/eco2/server/log"
     , template: "{date}.log"
     , fields: ["date", "time", "c-ip", "cs-username", "s-ip", "s-port", "cs-method", "cs-uri-stem", "cs-uri-query", "sc-status", "cs(User-Agent)"]
 });
-}
+
 
 
